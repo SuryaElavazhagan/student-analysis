56aa9a2a5eaf52cf9108bbd7df950ff7d63738e9
surya.e <surya.e@zohocorp.com>
2020-10-04 09:58:04 +0530

    chore: linted

--- src/views/ArrearAnalysis.tsx
+++ src/views/ArrearAnalysis.tsx
@@ -96,6 +96,8 @@ export function ArrearAnalysis() {
 
   useEffect(() => {
     renderChart();
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isStandingArrear, arrearReport]);
 
   return (
